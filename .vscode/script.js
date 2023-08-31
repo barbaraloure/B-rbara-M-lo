a3703a2f734e75074bd0fca64bb7641731209d24
Bárbara Mélo <barbaraloure@outlook.com.br>
2023-08-31 15:45:51 -0300

    :tada: intial commit

---
+++ .vscode/script.js
@@ -0,0 +1,22 @@
+function toggleMode () {
+const html = document.documentElement
+html.classList.toggle('light')
+
+//pegar a tag image
+const img = document.querySelector("profile img")
+
+//substituir a imagem
+if(html.classList.contains('light')) {
+//se tiver light mode, adicionar a imagem light
+img.setAttribute('src', './assets/avatar-light.png')
+} else {
+// se tiver sem light mode, manter a imagem normal
+img.setAttribute('src', './assets/IMG_20211201_144447_265[4821] foto barbara.jpg')
+}
+}
+
+ 
+
+
+
+// se tiver sem light mode, manter a imagem normal
