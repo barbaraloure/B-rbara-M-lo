513dab6deac528615d116b0988209fbbbcf4afaa
Bárbara Mélo <barbaraloure@outlook.com.br>
2023-09-10 17:06:07 -0300

    :tada: itnitial commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -3,7 +3,7 @@ const html = document.documentElement
 html.classList.toggle('light')
 
 //pegar a tag image
-const img = document.querySelector("profile img")
+const img = document.querySelector("#profile img")
 
 //substituir a imagem
 if(html.classList.contains('light')) {
